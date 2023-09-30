3cb65f1484fad6f744a7b036802748ec3e231475
Crowdbotics <team@crowdbotics.com>
2023-09-30 19:59:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "serviexpress-43678",
+  url: "https://serviexpress-43678.botics.co",
+  api: "https://serviexpress-43678.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
